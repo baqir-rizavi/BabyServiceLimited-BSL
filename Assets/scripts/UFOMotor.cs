4a5dbfd3c438d93e9470324b81872deaa83388c9
Baqir Rizavi <70775533+baqir-rizavi@users.noreply.github.com>
2022-09-07 10:48:09 -0700

    bullet system (#20)
    * ufo-added ui-Logo-added path-fixed
    
    * ufo-added ui-Logo-added path-fixed
    
    Co-authored-by: DON <abbeysaley@masti.com>

--- Assets/scripts/UFOMotor.cs
+++ Assets/scripts/UFOMotor.cs
@@ -6,12 +6,40 @@ public class UFOMotor : MonoBehaviour
 {
 
     [SerializeField] Transform target;
-    [SerializeField] float smoothness;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] float movementSmoothness = 1f;
+    [SerializeField] float ufoHeight = 5f;
+    
+    [SerializeField] float timeGapInShooting = 1.5f;
+
+    Transform tempBullet;
+
+    void Start()
+    {
+        StartCoroutine(shooting());
+    }
+
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 nextPos = new Vector3(target.position.x, transform.position.y, target.position.z);
-        transform.position = Vector3.Lerp(transform.position, nextPos, 1f/smoothness * Time.deltaTime);
+        Vector3 nextPos = new Vector3(target.position.x, ufoHeight, target.position.z);
+        transform.position = Vector3.Lerp(transform.position, nextPos, 1f/movementSmoothness * Time.deltaTime);
+        if (tempBullet != null)
+        {
+            tempBullet.Translate((target.position - transform.position) * bulletSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    IEnumerator shooting()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeGapInShooting);
+            GameObject b = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            tempBullet = b.transform;
+            Destroy(b, 2f);
+        }
     }
 }
